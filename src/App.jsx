a70ba40701a0c836f485e884ff28c73d2576a5eb
Kerciu <kerciuuu@gmail.com>
2024-10-05 17:11:58 +0200

    created navigation bar component skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Navbar from './Components/navbar/Navbar';
 import './App.css'
 
 function App() {
-  return <h1>abc</h1>;
+  return <div>
+    <Navbar/>
+  </div>;
 }
 
 export default App
