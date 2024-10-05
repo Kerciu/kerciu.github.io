540835ab1c472e00c51520dfb3c220344c3022db
Kerciu <kerciuuu@gmail.com>
2024-10-05 17:22:39 +0200

    added navigation bar contents and created styles for them

--- src/Components/navbar/Navbar.jsx
+++ src/Components/navbar/Navbar.jsx
@@ -2,9 +2,17 @@ import React from "react";
 import './Navbar.css';
 
 const Navbar = () => {
-    return
-    <div className="navbar">
-
+    return <div className="navbar">
+        <ul className="nav-menu">
+            <li>Home</li>
+            <li>About me</li>
+            <li>Tech stack</li>
+            <li>Portfolio</li>
+            <li>Contact</li>
+        </ul>
+        <div className="nav-connect">
+            Connect With Me
+        </div>
     </div>
 };
 
