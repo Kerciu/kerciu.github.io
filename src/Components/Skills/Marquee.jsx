4fac63685e97a45f122350adb0c245457d822fc4
Kerciu <kerciuuu@gmail.com>
2024-10-06 11:31:12 +0200

    managed to create sliding cards effect on marquee

--- src/Components/Skills/Marquee.jsx
+++ src/Components/Skills/Marquee.jsx
@@ -1,15 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 
 import './Marquee.css';
 
 import marqueeCards from "../../assets/info/marqueeCards";
 
 const Marquee = () => {
+
+    const [isPaused, setPaused] = useState(false);
+
     return (
-        <div className="marquee-container">
-            <div className="marquee-content">
+        <div className="marquee-container"
+        onMouseEnter={() => setPaused(true)}
+        onMouseLeave={() => setPaused(false)}>
+            <div className={`marquee-content ${isPaused ? 'paused' : ''}`}>
                 {marqueeCards.map(item => (
                     <div key={item.id} className="item">
+                        <img src={item.iconPath} alt={item.label} />
+                        <div className="label">{item.label}</div>
+                    </div>
+                ))}
+                {marqueeCards.map(item => (
+                    <div key={item.id + '-duplicate'} className="item">
                         <img src={item.iconPath} alt={item.label} />
                         <div className="label">{item.label}</div>
                     </div>
