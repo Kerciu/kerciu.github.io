0be31876aa242c9666374c726bb3a23805c3e167
Kerciu <kerciuuu@gmail.com>
2024-10-06 10:20:42 +0200

    created initial about section along with its styles

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,9 +1,26 @@
 import React from "react";
-import './About';
+import './About.css';
+
+import profileImage from '../../assets/my-image.jpg'
 
 const About = () => {
     return (
-        <div className="about"></div>
+        <div className="about-container">
+            <div className="about-title">
+                <h1>About me</h1>
+            </div>
+            <div className="about">
+                <div className="about-left">
+                    <img src={profileImage} alt="My image" />
+                </div>
+                <div className="about-right">
+                    <div className="about-paragraph">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur odio culpa nobis dolore, alias pariatur?</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita adipisci commodi in! Ab assumenda, molestiae aperiam earum dolorem atque quo.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
