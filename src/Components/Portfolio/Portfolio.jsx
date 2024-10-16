577c6ebdc726bdc906facd0b3837c8f6d5d0818a
Kerciu <Kerciuuu@gmail.com>
2024-10-16 20:50:46 +0200

    added show more button in the portfolio section redirecting to the github page

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -6,6 +6,8 @@ import projectInfo from "../../assets/info/projectInfo";
 import arrowIcon from "../../assets/arrow_icon.svg";
 
 const Portfolio = () => {
+    const githubUrl = "https://github.com/Kerciu";
+
     return (
         <div className="portfolio-container">
             <div className="portfolio-title">
@@ -18,12 +20,16 @@ const Portfolio = () => {
                         <h3>{project.pNum}</h3>
                         <h2>{project.pName}</h2>
                         <p>{project.pDesc}</p>
-                        <div className="project-srccode">
+                        <div className="project-btn">
                             <p>Source Code</p>
                             <a href={project.pLink} target="_blank"><img src={arrowIcon} alt="Arrow Icon" /></a>
                         </div>
                     </div>
                 })}
+            </div>
+            <div className="project-btn show-more-btn">
+                <p>Show More</p>
+                <a href={githubUrl} target="_blank"><img src={arrowIcon} alt="Arrow Icon" /></a>
             </div>
         </div>
     )
