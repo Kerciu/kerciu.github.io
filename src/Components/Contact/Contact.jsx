8ee7e9effb63399711e2adfac503b2407d5ff68e
Kerciu <Kerciuuu@gmail.com>
2024-10-16 21:04:26 +0200

    finished html structure of the left side in the contact section

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,9 +1,39 @@
 import React from "react";
 import './Contact.css';
 
+import themePattern from "../../assets/theme_pattern.svg";
+import mailIcon from "../../assets/mail_icon.svg";
+import callIcon from "../../assets/call_icon.svg";
+import locationIcon from "../../assets/location_icon.svg";
+
+import contactInfo from "../../assets/info/contactInfo";
+
 const Contact = () => {
     return (
-        <div> </div>
+        <div className="contact">
+            <div className="contact-title">
+                <h1>Contact</h1>
+                <img src={themePattern} alt="Theme Pattern" />
+            </div>
+            <div className="contact-section">
+                <div className="contact-left">
+                    <h1>Let's get in touch</h1>
+                    <p>{contactInfo.contactDesc}</p>
+                    <div className="contact-detail">
+                        <img src={locationIcon} alt="Location Icon" />
+                        <p>{contactInfo.location}</p>
+                    </div>
+                    <div className="contact-detail">
+                        <img src={mailIcon} alt="Mail Icon" />
+                        <p>{contactInfo.mail}</p>
+                    </div>
+                    <div className="contact-detail">
+                        <img src={callIcon} alt="Call Icon" />
+                        <p>{contactInfo.call}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
---
+++ src/assets/info/contactInfo.js
@@ -0,0 +1,8 @@
+const contactInfo = {
+    contactDesc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet quod quo aperiam error nulla adipisci asperiores. Nemo rerum impedit voluptates.",
+    location: "Warsaw, Poland",
+    mail: "kacper.gorski.contact@gmail.com",
+    call: "+48 213 769 420"
+}
+
+export default contactInfo;
