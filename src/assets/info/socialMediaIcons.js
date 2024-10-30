acea3fdee85609c41dce67f587c5b2e5fe3830ca
Kerciu <Kerciuuu@gmail.com>
2024-10-30 11:32:34 +0100

    preparing social media icons section for the contact details

---
+++ src/assets/info/socialMediaIcons.js
@@ -0,0 +1,10 @@
+const filePath = '../../assets/social_icons/';
+
+socialMediaIcons = {
+    facebook: filePath+'facebook.png',
+    github: filePath+'github.png',
+    instagram: filePath+'instagram.png',
+    linkedin: filePath+'linkedin.png',
+}
+
+export default socialMediaIcons;
