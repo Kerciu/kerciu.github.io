c8b0971fad4f317ac07b6bec30aba1060a9cf614
Kerciu <Kerciuuu@gmail.com>
2024-10-30 11:37:05 +0100

    filled social media icons object with objects that have their file path and link

--- src/assets/info/socialMediaIcons.js
+++ src/assets/info/socialMediaIcons.js
@@ -1,10 +1,26 @@
 const filePath = '../../assets/social_icons/';
 
 socialMediaIcons = {
-    facebook: filePath+'facebook.png',
-    github: filePath+'github.png',
-    instagram: filePath+'instagram.png',
-    linkedin: filePath+'linkedin.png',
+    facebook: {
+        path: filePath+'facebook.png',
+        link: "https://www.facebook.com/kxcper.gorski"
+    },
+    github:  {
+        path: filePath+'github.png',
+        link: "https://github.com/Kerciu"
+    },
+    instagram:  {
+        path: filePath+'instagram.png',
+        link: "https://www.instagram.com/kxcper.gorski/"
+    },
+    linkedin:  {
+        path: filePath+'linkedin.png',
+        link: "https://www.linkedin.com/in/kacper-gorski-se/"
+    },
+    twitter:  {
+        path: filePath+'twitter.png',
+        link: "https://x.com/Kerciuu"
+    },
 }
 
 export default socialMediaIcons;
