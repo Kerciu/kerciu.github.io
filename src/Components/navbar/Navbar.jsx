427e997baabb12c4e369e5ffb82564b1fa26edc6
Kerciu <kerciuuu@gmail.com>
2025-02-06 14:47:00 +0100

    started creating side menu for the navigation bar

--- src/Components/navbar/Navbar.jsx
+++ src/Components/navbar/Navbar.jsx
@@ -1,16 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-scroll";
 import './Navbar.css';
 
 import themePattern from "../../assets/theme_pattern.svg";
+import menuOpen from "../../assets/menu_open.svg";
+import menuClose from "../../assets/menu_close.svg";
 
 const Navbar = () => {
+
+
     return <div className="navbar">
         <div className="nav-logo">
             <div className="nav-logo-name">Kacper</div>
             <img src={themePattern} alt="Theme Pattern" />
         </div>
+        <img src={menuOpen} alt="Hamburger Menu" className="nav-mobile-open"/>
         <ul className="nav-menu">
+            <img src={menuClose} alt="Close Hamburger Menu" className="nav-mobile-close" />
             <li>
                 <Link to="home" smooth={true} duration={500}>Home</Link>
             </li>
@@ -27,7 +33,7 @@ const Navbar = () => {
                 <Link to="contact" smooth={true} duration={500}>Contact</Link>
             </li>
         </ul>
-        <div className="nav-connect"> 
+        <div className="nav-connect">
             <Link to="contact" smooth={true} duration={500}>Connect With Me</Link>
         </div>
     </div>
